2ff219266a3f1e1f079744265fc6bac2b16b1735
cweinberger <christian.weinberger@me.com>
2018-08-05 14:05:36 +0200

    Specify revision of paginator until released version is available

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Using my fork until new release (0.1.0) has been created by ryan.
 //        .package(url: "https://github.com/ryangrimm/VaporElasticsearch.git", .upToNextMinor(from: "0.1.0"))
         .package(url: "https://github.com/cweinberger/VaporElasticsearch.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/nodes-vapor/paginator.git", .branch("vapor-3"))
+        .package(url: "https://github.com/nodes-vapor/paginator.git", .revision("945cf5a568bbf0a423f2e43a9b5300f03c7efd60"))
     ],
     targets: [
         .target(
